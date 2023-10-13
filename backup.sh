7a45ab9299248e9dc5e55cd17c3f7c2ee8050e79
José Calderón <jgcalderon372@gmail.com>
2023-10-12 23:59:27 -0500

    Hash para verificar

--- backup.sh
+++ backup.sh
@@ -39,7 +39,10 @@ OPCIONES_SCP="-o PasswordAuthentication=no"
 
 # Rutas de directorio
 ORIGEN_DIR="/home/$USUARIO"
-DESTINO_DIR="/home/DPAdmin16-2023#66"
+DESTINO_DIR="/home/$USUARIO/DPAdmin16-2023#66"
+
+# Calcular el hash SHA-256 del contenido antes del cifrado en el origen
+SHA256_PRE_ENCRYPT=$(tar cz "$ORIGEN_DIR" | sha256sum | awk '{print $1}')
 
 # Comprimir y cifrar la copia de seguridad en el servidor origen
 tar czf - "$ORIGEN_DIR" | gpg --batch --symmetric --passphrase "$PASSPHRASE" | ssh $OPCIONES_SCP $USUARIO@$ORIGEN "cat > /tmp/backup.tar.gz.gpg"
@@ -50,8 +53,15 @@ scp $OPCIONES_SCP $USUARIO@$ORIGEN:/tmp/backup.tar.gz.gpg "$DESTINO:$DESTINO_DIR
 # Descomprimir y descifrar la copia de seguridad en el servidor destino
 ssh $DESTINO "cd $DESTINO_DIR && gpg --batch --decrypt --passphrase $PASSPHRASE -o backup.tar.gz /tmp/backup.tar.gz.gpg && tar xzf backup.tar.gz"
 
+# Calcular el hash SHA-256 del contenido después del descifrado en el destino
+SHA256_POST_DECRYPT=$(tar cz "$DESTINO_DIR" | sha256sum | awk '{print $1}')
+
 # Limpiar archivos temporales en el servidor origen y destino
 ssh $USUARIO@$ORIGEN "rm /tmp/backup.tar.gz.gpg"
 ssh $DESTINO "rm $DESTINO_DIR/backup.tar.gz"
+
+# Verificar los hash SHA-256 antes y después de la transferencia
+echo "Hash SHA-256 antes del cifrado: $SHA256_PRE_ENCRYPT"
+echo "Hash SHA-256 después del descifrado: $SHA256_POST_DECRYPT"
 
 echo "Copia de seguridad completa."
